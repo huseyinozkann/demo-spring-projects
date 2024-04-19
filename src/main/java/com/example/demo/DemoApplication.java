e7d9637664dd59bc4870af70c4e47efc572f4538
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-04-20 00:49:08 +0300

    Student Hello added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -15,8 +20,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/")
-	public String hello(){
-		return "Hello Spring";
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Hüseyin",
+						"ozkan.huseyin@gmail.com",
+						LocalDate.of(2001, Month.APRIL,23),
+						21
+				)
+		);
 	}
 
 }
