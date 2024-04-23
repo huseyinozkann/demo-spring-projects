484bf4a40f76ed6bf206364a1bd1b7de7a0df247
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-04-24 02:06:55 +0300

    Student Repository and Student Config added

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+
+            Student hso = new Student(
+                    "Hüseyin",
+                    "ozkan.huseyin@gmail.com",
+                    LocalDate.of(2001, APRIL,23),
+                    21
+            );
+
+            Student mustafa = new Student(
+                    "Mustafa",
+                    "ozkan.mustafa@gmail.com",
+                    LocalDate.of(1995, APRIL,20),
+                    29
+            );
+            repository.saveAll(
+                    List.of(hso, mustafa)
+            );
+        };
+    }
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -10,15 +11,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Hüseyin",
-                        "ozkan.huseyin@gmail.com",
-                        LocalDate.of(2001, Month.APRIL,23),
-                        21
-                )
-        );
+       return studentRepository.findAll();
     }
 }
